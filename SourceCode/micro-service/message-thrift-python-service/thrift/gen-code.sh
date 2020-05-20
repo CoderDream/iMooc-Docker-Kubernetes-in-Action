be1ed9023a3fd92bfcfbc13a935b754e3f53dadb
xulin28709 <xulin28709@billjc.com>
2020-05-20 11:09:57 +0800

    配置maven 202005201109

--- SourceCode/micro-service/message-thrift-python-service/thrift/gen-code.sh
+++ SourceCode/micro-service/message-thrift-python-service/thrift/gen-code.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
-thrift --gen py -out ../ message.thrift
+#thrift --gen py -out ../ message.thrift
+thrift --gen java -out ../../message-thirft-service-api/src/main/java message.thrift
